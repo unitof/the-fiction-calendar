3fc21dede22ee499ab0c15273f8d2d2ec8f23088
Jacob Ford <j@cobford.com>
2020-03-30 16:33:25 -0400

    Clean output folder each build

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,16 @@
-module.exports = {
-  dir: {
-    input: 'content'
+const del = require('del')
+const outputDir = '_site'
+
+module.exports = (eleventyConfig) => {
+
+  // auto-clean output file
+  console.log(eleventyConfig)
+  const dirToClean = outputDir + '/*'
+  del(dirToClean)
+
+  return {
+    dir: {
+      input: 'content'
+    }
   }
 }
