7268789f5a630a41fd94235dae05d95c16065040
Jacob Ford <j@cobford.com>
2020-03-31 01:04:11 -0400

    Remove .gitkeep from events dir
    Feels sloppy and exceptiony and not gonna be empty no more!

--- tests/events.test.js
+++ tests/events.test.js
@@ -3,8 +3,6 @@ const path = require('path')
 
 const eventsDir = path.join(__dirname, '../content/events')
 const eventFilenames = readdirSync(eventsDir)
-                       .filter(filename => filename !== '.gitkeep')
-                       // ignore .gitkeep file (it is allowed)
 
 describe.each(eventFilenames)('event filenames', filename => {
   test('must have format `YYYY-MM-DD event-slug.yaml`', () => {
